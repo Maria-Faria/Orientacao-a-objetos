30dfe8155cd15e9d0bdf15a0a3b25746b9e3f56f
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-02-16 00:37:11 -0300

    exercício 5 - classe Aluno

---
+++ Ex5/Main.java
@@ -0,0 +1,13 @@
+package Ex5;
+
+public class Main {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno("Maria Eduarda", 19);
+        aluno1.exibeInfo();
+
+        System.out.println();
+
+        Aluno aluno2 = new Aluno("José Bezerra", 21);
+        aluno2.exibeInfo();
+    }
+}
